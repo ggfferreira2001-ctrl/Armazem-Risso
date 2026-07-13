ed749914f8fbeb313af5f91e702a4edf3cce3906
ggfferreira2001-ctrl <ggf.ferreira2001@gmail.com>
2026-07-12 21:06:21 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -16,7 +16,7 @@
  */
 
 // Suba este número toda vez que quiser forçar a atualização do cache.
-const VERSAO_CACHE = 'armazem-risso-v7';
+const VERSAO_CACHE = 'armazem-risso-v8';
 
 const ARQUIVOS_ESTATICOS = [
   './',
